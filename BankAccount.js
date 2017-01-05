8be769cabcf6df9f67955866bfd64b69f82a6515
irwinpratajaya <irwinpratajaya@gmail.com>
2017-01-05 11:47:58 +0700

    Bank Account

--- BankAccount.js
+++ BankAccount.js
@@ -5,18 +5,35 @@ const ACCT_NUMBER = new WeakMap()
 class BankAccount {
   constructor(customer_name, type, acct_number) {
     //
+    this.customer_name = customer_name
+    this.type = type
+    ACCT_NUMBER.set(this, acct_number)
+
+  }
+
+  set accountNumber(num){
+    ACCT_NUMBER.set(this, num)
   }
 
   get account_number() {
     //
+    return ACCT_NUMBER.get(this)
   }
 
   to_s() {
     //
+    console.log( this.customer_name + ": " + this.type + "# " + ACCT_NUMBER.get(this) )
   }
 
   cover_digits() {
     //
+    let splitAccNum =  ACCT_NUMBER.get(this).split('-')
+    console.log(splitAccNum)
+    let hideNum = splitAccNum.splice(0, 2, '***','-','***', '-')
+    // console.log(splitAccNum.join(' '))
+
+
+    console.log (( this.customer_name + ": " + this.type + "# " + splitAccNum.join('') ))
   }
 }
 
