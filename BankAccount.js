201851f29cadfa4d158e2a4d6b0ab106c20a216a
Wahyuhidayatt <wahyuhdyt95@gmail.com>
2017-01-05 12:07:58 +0700

    BankAccount

--- BankAccount.js
+++ BankAccount.js
@@ -4,19 +4,25 @@ const ACCT_NUMBER = new WeakMap()
 
 class BankAccount {
   constructor(customer_name, type, acct_number) {
-    //
+    this.customer_name = customer_name;
+    this.type = type;
+    ACCT_NUMBER.set(this, acct_number);
+
   }
 
   get account_number() {
-    //
+    return ACCT_NUMBER.get(this)
   }
 
   to_s() {
-    //
+    return `${this.customer_name} : ${this.type}# ${ACCT_NUMBER.get(this)}`
   }
 
   cover_digits() {
     //
+    var digit = this.account_number;
+    digit = digit.replace(/\d{3}(?=\-)/g,'***')
+    return `${this.customer_name} : ${this.type} ${digit}`
   }
 }
 
@@ -26,7 +32,7 @@ console.log(my_acct)
 console.log(my_acct.account_number)
 
 // release 0
-my_acct.to_s() // "Hacktivate: Checking# 333-555-888"
+console.log(my_acct.to_s()) // "Hacktivate: Checking# 333-555-888"
 
 // release 1
-my_acct.cover_digits() // "Hacktivate: Checking# ***-***-888"
+console.log(my_acct.cover_digits()) // "Hacktivate: Checking# ***-***-888"
