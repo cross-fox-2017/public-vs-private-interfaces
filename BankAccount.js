99a28ba7f3ff093d85fea29f136b499dfd924c32
Isumi & Muhammad Firman Pebrizal <pair+isumizumi.mfpebrizal@devbootcamp.com>
2017-01-05 11:47:11 +0700

    Done

--- BankAccount.js
+++ BankAccount.js
@@ -4,26 +4,28 @@ const ACCT_NUMBER = new WeakMap()
 
 class BankAccount {
   constructor(customer_name, type, acct_number) {
-    //
+    this._customer_name = customer_name
+    this._type = type
+    this._acct_number = acct_number
   }
 
-  get account_number() {
-    //
+  account_number() {
+    return this._acct_number
   }
 
   to_s() {
-    //
+    return console.log(`${this._customer_name}: ${this._type}# ${this._acct_number}`)
   }
 
   cover_digits() {
-    //
+    return console.log(`${this._customer_name}: ${this._type}# ${this._acct_number.replace(/\d{3}-\d{3}/, "***-***")}`)
   }
 }
 
 let my_acct = new BankAccount("Hacktivate", "Checking", "333-555-888")
 
 console.log(my_acct)
-console.log(my_acct.account_number)
+// console.log(my_acct.account_number())
 
 // release 0
 my_acct.to_s() // "Hacktivate: Checking# 333-555-888"
