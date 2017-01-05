30c41e4b68360fa7953ecef30cdee377da2dc347
Eri Irawan & Raditya Arya <pair+mrei91.radityaarya@hacktiv8.com>
2017-01-05 10:57:12 +0700

    selesai release 0 radit_eri

--- BankAccount.js
+++ BankAccount.js
@@ -4,15 +4,41 @@ const ACCT_NUMBER = new WeakMap()
 
 class BankAccount {
   constructor(customer_name, type, acct_number) {
-    //
+    //ACCT_NUMBER.set(this, acct_number);
+    this._customer_name = customer_name;
+    this._type          = type;
+    this._acct_number   = acct_number;
   }
 
-  get account_number() {
-    //
+  // customer name
+  set setCustomer_name(valName){
+    this._customer_name = valName;
+  }
+
+  get getCustomer_name(){
+    return getCustomer_name.customer_name;
+  }
+
+  // type
+  set setType(valType){
+    this._type = valType;
+  }
+
+  get getType(){
+    return getType.type;
+  }
+
+  // acc number
+  set setAccount_number(valNumber){
+    this._acct_number = valNumber;
+  }
+
+  get getAccount_number() {
+    return getAcct_number.acct_number;
   }
 
   to_s() {
-    //
+    return `${this._customer_name} : ${this._type}# ${this._acct_number}`;
   }
 
   cover_digits() {
@@ -23,10 +49,10 @@ class BankAccount {
 let my_acct = new BankAccount("Hacktivate", "Checking", "333-555-888")
 
 console.log(my_acct)
-console.log(my_acct.account_number)
+console.log(my_acct._acct_number)
 
 // release 0
-my_acct.to_s() // "Hacktivate: Checking# 333-555-888"
+console.log(my_acct.to_s()) // "Hacktivate: Checking# 333-555-888"
 
 // release 1
-my_acct.cover_digits() // "Hacktivate: Checking# ***-***-888"
+// my_acct.cover_digits() // "Hacktivate: Checking# ***-***-888"
