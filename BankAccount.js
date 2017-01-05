639eaaeceed52f2ef0dcd800ee28da7cb13f678a
Syanmil <syanmil@yahoo.com>
2017-01-05 10:45:04 +0700

    release 0

--- BankAccount.js
+++ BankAccount.js
@@ -4,7 +4,9 @@ const ACCT_NUMBER = new WeakMap()
 
 class BankAccount {
   constructor(customer_name, type, acct_number) {
-    //
+    this.customer_name = customer_name;
+    this.type = type;
+    this._acct_number = acct_number
   }
 
   get account_number() {
@@ -12,18 +14,26 @@ class BankAccount {
   }
 
   to_s() {
-    //
+    return console.log(`${this.customer_name}: ${this.type}# ${this._acct_number}`)
   }
 
   cover_digits() {
     //
+  }
+
+  set setAccNumber(val){
+    this._acct_number = val;
+  }
+
+  get getAccNumber(){
+    return this._acct_number;
   }
 }
 
 let my_acct = new BankAccount("Hacktivate", "Checking", "333-555-888")
 
 console.log(my_acct)
-console.log(my_acct.account_number)
+console.log(my_acct.getAccNumber)
 
 // release 0
 my_acct.to_s() // "Hacktivate: Checking# 333-555-888"
