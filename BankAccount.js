b817e9f9d6259f056c19b534db040d03dc61f3ca
pisanggoreng <7ofpentacles@gmail.com>
2017-01-08 21:09:10 +0700

    release 0

--- BankAccount.js
+++ BankAccount.js
@@ -1,18 +1,37 @@
 "use strict"
-
-const ACCT_NUMBER = new WeakMap()
+const COSTUMER_NAME = new WeakMap();
+const TYPE = new WeakMap()
+const ACCT_NUMBER = new WeakMap();
 
 class BankAccount {
   constructor(customer_name, type, acct_number) {
-    //
+    COSTUMER_NAME.set(this, customer_name);
+    TYPE.set(this, type);
+    ACCT_NUMBER.set(this, acct_number)
   }
 
-  get account_number() {
-    //
+  set setCostumerName(costumer_name) {
+    COSTUMER_NAME.set(this, customer_name);
+  }
+  set setType(type) {
+    TYPE.set(this, type);
+  }
+  set setAcctNumber(acct_number) {
+    ACCT_NUMBER.set(this, acct_number)
+  }
+
+  get setCostumerName() {
+    return COSTUMER_NAME.get(this);
+  }
+  get setType() {
+    return TYPE.get(this);
+  }
+  get setAcctNumber() {
+    return ACCT_NUMBER.get(this)
   }
 
   to_s() {
-    //
+    console.log( `${COSTUMER_NAME.get(this)}: ${TYPE.get(this)} # ${ACCT_NUMBER.get(this)}` ); 
   }
 
   cover_digits() {
